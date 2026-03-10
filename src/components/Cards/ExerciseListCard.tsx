237a4ccfacba02756cbe4dde2d4e3a1ef0f333cb
Amy Pang <amypang@umich.edu>
2026-03-10 02:23:31 -0400

    removed unnecessary import

--- src/components/Cards/ExerciseListCard.tsx
+++ src/components/Cards/ExerciseListCard.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import type { Exercise } from "../../types/exercise";
 import { supabase } from "../../supabaseClient";
 import ExerciseCard from "./ExerciseCard";
